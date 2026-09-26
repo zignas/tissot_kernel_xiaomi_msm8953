38b92dfcebfe3a7b4a526ef4236c653dc69cf40f
zignas <zignas@outlook.com>
2026-09-26 04:32:00 +0300

    drivers: power: supply: qcom: smbchg: ist3088c touchscreen
    exclude "ist30xx_set_ta_mode" symbol from non-mido targets

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -4456,22 +4456,23 @@ static int smbchg_restricted_charging(struct smbchg_chip *chip, bool enable)
 	return rc;
 }
 
-#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8953)
-extern void ist30xx_set_ta_mode(bool mode);
 int set_usb_charge_mode_par = 0;
+
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MIDO)
+extern void ist30xx_set_ta_mode(bool mode);
 #endif
 
 static void handle_usb_removal(struct smbchg_chip *chip)
 {
-	struct power_supply *parallel_psy = get_parallel_psy(chip);
-	union power_supply_propval pval = {0, };
-	int rc;
+    struct power_supply *parallel_psy = get_parallel_psy(chip);
+    union power_supply_propval pval = {0, };
+    int rc;
 
-#if IS_ENABLED(CONFIG_MACH_XIAOMI_MSM8953)
-	if (xiaomi_msm8953_mach_get() == XIAOMI_MSM8953_MACH_MIDO) {
-		if (set_usb_charge_mode_par == 1)
-			ist30xx_set_ta_mode(0);
-	}
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_MIDO)
+    if (xiaomi_msm8953_mach_get() == XIAOMI_MSM8953_MACH_MIDO) {
+        if (set_usb_charge_mode_par == 1)
+            ist30xx_set_ta_mode(0);
+    }
 #endif
 
 	pr_smb(PR_STATUS, "triggered\n");
